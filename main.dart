4b15142b8cd6399f9920781c1533c4024de1c430
Makara <makaragit.sam@2023.com>
2023-09-18 04:02:48 +0700

    week1 dart

---
+++ main.dart
@@ -0,0 +1,13 @@
+void main(){
+  var map ={'name':'Makara', 'gender':'male', 13:'2002'};
+  map['placeOfBirth'] = 'Siem Reap';
+
+  var intMap = <int,String>{};
+  intMap[0] = 'Value is 001';
+  intMap[1] = 'Value is 002';
+  intMap[2] = 'Value is 003';
+
+  print(map);
+  print(intMap);
+
+}
